fd4579c35c9ad1cfc07d27aaaa2fe81eaac58d14
UweDraeger <uwe.draeger@gmx.net>
2021-01-05 18:33:59 +0100

    changes to plot 4

--- plot4.R
+++ plot4.R
@@ -3,10 +3,19 @@ library(tidyverse)
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
-coal1 <- str_which(SCC$EI.Sector, "Coal|coal") # 99 obs
-coal2 <- str_which(SCC$Short.Name, "Coal|coal") # 239 obs
+# coal combustion as fuel
+fuel_comb <- str_which(SCC$EI.Sector, "Fuel Comb") # 530 obs
+coal_used <- str_which(SCC$EI.Sector, "Coal$") # 99 obs
 
-coal <- union(coal1, coal2) #251 obs
+fuel <- intersect(fuel_comb, coal_used)
+
+# ships - combusting coal to power ships (not included above)
+# assuming there are no coal-powered airplanes, locomotives, or road vehicles 
+vessels <- str_which(SCC$EI.Sector, "Vessels") # 44 obs
+coal_powered <- str_which(SCC$SCC.Level.Three, "Coal") # 172 obs
+ships <- intersect(vessels, coal_powered) # 5 obs
+
+coal <- union(fuel, vessels) # 143 obs
 
 x <- SCC[coal,1]
 
@@ -15,7 +24,7 @@ coal_combustion <- NEI %>%
         group_by(year) %>%
         summarise(total = sum(Emissions) / 1000)
 
-png(filename = "plot4.png", width = 720)
+#png(filename = "plot4.png", width = 720)
 
 barplot(coal_combustion$total, 
         names.arg = c("1999", "2002", "2005", "2008"),
@@ -23,4 +32,4 @@ barplot(coal_combustion$total,
         main = "Total amount of PM2.5 emitted from coal combustion-related sources per year",
         sub = "Source: Environmental Protection Agency")
 
-dev.off()
+# dev.off()
